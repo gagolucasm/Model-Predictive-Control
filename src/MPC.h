68c13dee5b4de3223debca0120e017134e0c3610
Lucas <gagolucasm@hotmail.com>
2017-06-19 10:55:53 -0700

    Final Tunning

---
+++ src/MPC.h
@@ -0,0 +1,24 @@
+#ifndef MPC_H
+#define MPC_H
+
+#include <vector>
+#include "Eigen-3.3/Eigen/Core"
+
+using namespace std;
+
+class MPC {
+ public:
+  MPC();
+
+  virtual ~MPC();
+
+  // Solve the model given an initial state and polynomial coefficients.
+  // Return the first actuatotions.
+  vector<double> Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs);
+
+  vector<double> x_vals;
+  vector<double> y_vals;
+  double last_steering;
+};
+
+#endif /* MPC_H */
